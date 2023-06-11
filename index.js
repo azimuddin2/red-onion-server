ae88e49cdcbebcbfa258f85e52c5130658073324
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-06-11 23:38:25 +0600

    get operation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,41 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const categories = require('./data/categories.json');
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t3bn8t4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        const foodCollection = client.db("redOnion").collection("foods");
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {category_id : id};
+            const foods = await foodCollection.find(query).toArray();
+            res.send(foods);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
