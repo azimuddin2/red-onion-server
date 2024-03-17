be8fa15d23282506d0546323d1f61418c7dbbc23
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-03-17 14:31:49 +0600

    cart related delete and patch api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -64,6 +64,29 @@ async function run() {
             }
             const query = { email: email };
             const result = await cartsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.patch('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateQuantity = req.body;
+
+            const filter = { _id: new ObjectId(id) };
+
+            const updateDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity,
+                },
+            };
+
+            const result = await cartsCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartsCollection.deleteOne(query);
             res.send(result);
         });
 
